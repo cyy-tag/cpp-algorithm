3b4d3d29c4b24fcf22c64136055c17e3030969d0
YingYinChen <chen15816602944@163.com>
2024-06-16 19:42:28 +0800

    bittree add range query api

--- src/tree/bit_index_tree.h
+++ src/tree/bit_index_tree.h
@@ -15,19 +15,29 @@ public:
     int lowbit(int x) {
         return x & -x;
     }
-
+    //x>0
     void add(int x, int c) {
         for(int i = x; i <= n_; i+=lowbit(i)) {
             bit_[i] += c;
         }
     }
 
-    // @param x > 0
     int query(int x) {
+        if(x == 0) {
+            return 0;
+        }
         int res = 0;
         for(int i = x; i; i -= lowbit(i)) {
             res += bit_[i];
         }
         return res;
+    }
+
+    //查询区间和[l, r]
+    int query(int l, int r) {
+        if(r < l) {
+            return 0;
+        }
+        return query(r) - query(l-1);
     }
 };
