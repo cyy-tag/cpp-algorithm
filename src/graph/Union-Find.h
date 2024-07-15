80e3d52f44335e492ac67587524400badd8a542c
YingYinChen <chen15816602944@163.com>
2024-07-15 22:54:36 +0800

    add union find

---
+++ src/graph/Union-Find.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <vector>
+
+class UnionFind
+{
+public:
+  UnionFind(int v)
+    :parent_(v), rank_(v, 1)
+  {
+    for(int i = 0; i < v; i++) {
+      parent_[i] = i;
+    }
+  }
+
+  int Find(int v) {
+    if(parent_[v] != v) {
+      return Find(parent_[v]);
+    }
+    return v;
+  }
+
+  void Union(int v, int q) {
+    int v_parent = Find(v);
+    int q_parent = Find(q);
+    if(v_parent == q_parent) {
+      return;
+    }
+
+    if(rank_[v_parent] > rank_[q_parent]) {
+      parent_[q_parent] = v_parent;
+      rank_[v_parent] += rank_[q_parent];
+    } else {
+      parent_[v_parent] = q_parent;
+      rank_[q_parent] += rank_[v_parent];
+    }
+  }
+
+private:
+  std::vector<int> parent_, rank_;
+};
