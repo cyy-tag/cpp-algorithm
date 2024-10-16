2a033869eed2bd6ce72ac1f63bf566c28048b542
yingyin.chen <chen15816602944@163.com>
2024-10-16 10:00:22 +0800

    add prime check

--- src/math/primer.h
+++ src/math/primer.h
@@ -1,1 +1,13 @@
 #pragma once
+
+bool is_prime(int n){
+    bool flag = true;
+    for(int i = 2;i<=n/i;i++){
+        if(n%i==0){
+            flag = false;
+            break;
+        }
+    }
+    return flag;
+}
+
