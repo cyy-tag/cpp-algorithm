f25f6f6cb5394e2a4dffd114364b8a18f6026090
yingyin.chen <chen15816602944@163.com>
2024-11-13 16:43:26 +0800

    update digit dp exercise

--- src/dp/digit_dp.h
+++ src/dp/digit_dp.h
@@ -1,11 +1,22 @@
 #pragma once
 #include <string>
+int a[20];
+
+void convert(long long num) {
+    int len = 0;
+    while(num) {
+        a[++len] = num % 10;
+        num /= 10;
+    }
+    //转换成十进制格式
+}
 
 using namespace std;
 // 返回从i 开始填数字, i 前面填的数字的集合是mask, 能构造出的特殊整数的数目
 // is_limit 表示前面填的数字是否都是n对应位上的, 如果为true，那么当前位至多为 int(s[i]), 否则至多为 9
 // is_num 表示前面是否填了数字(是否跳过), 如果为true, 那么当前位可以从0开始
 // 如果为false，那么我们可以跳过，或者从1开始填数字
+
 int CountSpecialNumbers(int n) {
     auto s = to_string(n);
     int m = s.length(), dp[m][1 << 10];
@@ -35,6 +46,18 @@ int CountSpecialNumbers(int n) {
  * 求后缀符合条件的个数, 后序遍历
  * https://codeforces.com/problemset/problem/507/D
  * 
- * 全局记忆化, 状态压缩
+ * 全局记忆化, 状态压缩, 全局记忆化需要特判 !is_limit和isNum前导零, 以及位数记忆化下标要从低位到高
  * https://codeforces.com/contest/855/problem/E
- */
+ * 
+ * // 位上计数、求和 [L, R]范围
+ * https://www.luogu.com.cn/problem/AT_abc029_d
+ * https://www.luogu.com.cn/problem/P2602
+ * https://www.luogu.com.cn/problem/P4999
+ * 
+ * 在给定范围查找[l, r] 第K个符合条件的 二分答案 + 数位dp
+ * https://www.luogu.com.cn/problem/AT_arc173_a
+ * 
+ * 统计回文串个数
+ * https://www.luogu.com.cn/problem/B3883
+ * 
+ * 
