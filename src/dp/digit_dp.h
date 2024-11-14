9eb1869051b56914d96f6d6f27620943977ee875
cyy <12394364+cyy233@user.noreply.gitee.com>
2024-11-14 21:13:24 +0800

    update digit dp code

--- src/dp/digit_dp.h
+++ src/dp/digit_dp.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <string.h>
 #include <string>
 int a[20];
 
@@ -19,24 +20,24 @@ using namespace std;
 
 int CountSpecialNumbers(int n) {
     auto s = to_string(n);
-    int m = s.length(), dp[m][1 << 10];
-    memset(dp, -1, sizeof(dp));
-    function<int(int, int, bool, bool)> f = [&](int i, int mask, bool isLimit, bool isNum) -> int {
+    int m = s.length(), memo[m][1 << 10];
+    memset(memo, -1, sizeof(memo));
+    auto dfs = [&](auto& dfs, int i, int mask, bool isLimit, bool isNum) -> int {
         if(i == m) return isNum;
-        if(!isLimit && isNum && dp[i][mask] >= 0) return d[i][mask];
+        if(!isLimit && isNum && memo[i][mask] != -1) return memo[i][mask];
         int res = 0;
-        if(!isNum) res = f(i+1, mask, false, false); //可以跳过当前数
+        if(!isNum) res = dfs(dfs, i+1, mask, false, false); //可以跳过当前数
         for(int d = 1 - isNum, up = isLimit ? s[i] - '0' : 9; d <= up; ++d) //枚举要填入的数字d
         {
             if ((mask >> d & 1) == 0) { // d不在mask中
-                res += f(i+1, mask | (1 << d), isLimit && d == up, true);
+                res += dfs(dfs, i+1, mask | (1 << d), isLimit && d == up, true);
             }
         }
 
-        if(!isLimit && isNum) dp[i][mask] = res;
+        if(!isLimit && isNum) memo[i][mask] = res;
         return res;
     };
-    return f(0, 0, true, false);
+    return dfs(dfs, 0, 0, true, false);
 }
 
 /*题目
