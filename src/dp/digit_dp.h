33b73bd48906109bf2287e45de9e33f4307da74d
cyy <12394364+cyy233@user.noreply.gitee.com>
2024-11-18 22:38:11 +0800

    add digit exercise

--- src/dp/digit_dp.h
+++ src/dp/digit_dp.h
@@ -69,7 +69,14 @@ int CountSpecialNumbers(int n) {
  * 统计回文串个数
  * https://www.luogu.com.cn/problem/B3883
  * 
+ * 步进数
+ * https://leetcode.cn/problems/count-stepping-numbers-in-range/description/
+ * https://www.luogu.com.cn/problem/P2657
+ * 
  * 提高
  * 记忆化状态表示练习
  * https://leetcode.cn/problems/count-the-number-of-powerful-integers/description/
+ * https://ac.nowcoder.com/acm/problem/50520
+ * 超时优化
+ * https://ac.nowcoder.com/acm/problem/50521
  */
